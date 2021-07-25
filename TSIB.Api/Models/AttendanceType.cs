33af2d548808e0a8345beae74e62d6d7a4e917ae
NestoDo <nesto.do@gmail.com>
2021-07-25 16:28:01 -0500

    adjustments

--- TSIB.Api/Models/AttendanceType.cs
+++ TSIB.Api/Models/AttendanceType.cs
@@ -15,6 +15,7 @@ namespace TSIB.Api.Models
         public int AttendanceTypeId { get; set; }
         public string Description { get; set; }
         public string ShortDescription { get; set; }
+        public int Order { get; set; }
         public int CreatedUser { get; set; }
         public int UpdatedUser { get; set; }
         public DateTime CreatedDate { get; set; }
